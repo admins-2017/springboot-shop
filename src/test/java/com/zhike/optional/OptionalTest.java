93442479a97451350035c8e507d5213cdef418f2
kangdongwei <1067905926@qq.com>
2021-04-07 17:11:12 +0800

    修改bug

--- src/test/java/com/zhike/optional/OptionalTest.java
+++ src/test/java/com/zhike/optional/OptionalTest.java
@@ -6,26 +6,26 @@ import java.util.Optional;
 
 public class OptionalTest {
 
-    @Test
-    public void testOptional(){
-//        构建空的Optional
-        Optional<String> empty = Optional.empty();
-//        ofNullable构建optional 参数为空不报错
-        Optional<String> t1 =Optional.ofNullable("a");
-//        of构建optional 如果参数为空直接抛出异常
-        Optional<String> t2 =Optional.of(null);
-
-
-//      如果empty为空 则不会执行括号中的内容 反之执行打印
-        t1.ifPresent(s-> System.out.println(s));
-
-//        如果t1 有值 则给s 赋值 如果t1 为空  则给s 赋默认值
-        t1.orElse("123");
-
-//        使用map进行数据处理
-        String t3  = t1.map(t -> t + "b").orElse("c");
-//        使用filter进行判断 返回boolean
-        t1.filter(t -> t.equals("a")).orElse("d");
-    }
+//    @Test
+//    public void testOptional(){
+////        构建空的Optional
+//        Optional<String> empty = Optional.empty();
+////        ofNullable构建optional 参数为空不报错
+//        Optional<String> t1 =Optional.ofNullable("a");
+////        of构建optional 如果参数为空直接抛出异常
+//        Optional<String> t2 =Optional.of(null);
+//
+//
+////      如果empty为空 则不会执行括号中的内容 反之执行打印
+//        t1.ifPresent(s-> System.out.println(s));
+//
+////        如果t1 有值 则给s 赋值 如果t1 为空  则给s 赋默认值
+//        t1.orElse("123");
+//
+////        使用map进行数据处理
+//        String t3  = t1.map(t -> t + "b").orElse("c");
+////        使用filter进行判断 返回boolean
+//        t1.filter(t -> t.equals("a")).orElse("d");
+//    }
 
 }
